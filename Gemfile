e80caa83512140ccb0764ff9bdd7bacc0878382e
”k.kei” <acidjannedoor@yahoo.co.jp>
2019-06-07 09:30:27 +0000

    finish rails04

--- Gemfile
+++ Gemfile
@@ -38,6 +38,9 @@ gem 'bootstrap-sass'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
+gem "jquery-rails"
+
+gem 'bcrypt'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+   validates :name, presence: true, length: { maximum: 15 }
+   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  
+  has_secure_password
+  validates :password, presence: true, length: { in: 8..32 },format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i }
 end
